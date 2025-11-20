1cf3a40ac805906974c5e3af9dbfc8d85a954fb7
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-11-20 16:31:09 +0700

    add Formulir screen composable with submit handling and navigation to Detail

--- app/src/main/java/com/example/praktikum7/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/praktikum7/view/uicontroller/Navigasi.kt
@@ -1,11 +1,14 @@
 package com.example.praktikum7.view.uicontroller
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikum7.model.dataJK.JenisK
 import com.example.praktikum7.viewmodel.SiswaViewModel
 
 enum class Navigasi {
@@ -24,5 +27,20 @@ fun SiswaApp(
             navController = navController,
             startDestination = Navigasi.Formulir.name,
 
+            modifier = Modifier.padding(paddingValues = isiRuang)){
+            composable(route = Navigasi.Formulir.name){
+                val konteks = LocalContext.current
+                FormSiswa(
+                    pilihanJK = JenisK.map {id -> konteks.resources.getString(id)},
+                    onSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(route = Navigasi.Detail.name)
+                    }
+                )
+            }
 
-    }
+
+
+
+
+        }
