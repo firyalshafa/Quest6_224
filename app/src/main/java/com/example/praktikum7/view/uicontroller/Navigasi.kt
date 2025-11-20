992391c39465b6946d1ed7f38b84f0dca2efeeed
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-11-20 16:27:33 +0700

    integrate Scaffold and navigation host with ViewModel state

--- app/src/main/java/com/example/praktikum7/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/praktikum7/view/uicontroller/Navigasi.kt
@@ -1,7 +1,9 @@
 package com.example.praktikum7.view.uicontroller
 
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.praktikum7.viewmodel.SiswaViewModel
@@ -15,4 +17,12 @@ enum class Navigasi {
 fun SiswaApp(
     modifier : Modifier,
     viewModel: SiswaViewModel = viewModel(),
-) {}
+) {
+    Scaffold { isiRuang->
+        val uiState = viewModel.statusUI.collectAsState()
+        navHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+
+
+    }
