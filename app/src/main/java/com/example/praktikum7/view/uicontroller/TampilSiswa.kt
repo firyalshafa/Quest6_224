db87523df1158ece7c51421676efc515f822c8d4
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-11-20 15:43:09 +0700

    Add Scaffold and TopAppBar for Data Siswa screen

--- app/src/main/java/com/example/praktikum7/view/uicontroller/TampilSiswa.kt
+++ app/src/main/java/com/example/praktikum7/view/uicontroller/TampilSiswa.kt
@@ -2,11 +2,17 @@ package com.example.praktikum7.view.uicontroller
 
 
 
+import android.R
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import com.example.praktikum7.model.Siswa
 import java.lang.reflect.Modifier
@@ -22,5 +28,12 @@ fun TampilSiswa(
         Pair(first = stringResource(id = "Jenis Kelamin"), second = statusUISiswa.gender),
         Pair(first = stringResource(id = "Alamat"), second = statusUISiswa.alamat)
     )
+
+    Scaffold(modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource("Data Siswa"), color = R.color.white) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = colorResource(id = R.color.purple_500)),
+            )
 
 }
