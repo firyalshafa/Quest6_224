6b66c505b3805c068097027d7c096bde1daaeeba
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-11-20 16:22:17 +0700

    add SiswaApp composable with ViewModel setup

--- app/src/main/java/com/example/praktikum7/view/uicontroller/Navigasi.kt
+++ app/src/main/java/com/example/praktikum7/view/uicontroller/Navigasi.kt
@@ -1,6 +1,18 @@
 package com.example.praktikum7.view.uicontroller
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.praktikum7.viewmodel.SiswaViewModel
+
 enum class Navigasi {
     Formulir,
     Detail
 }
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SiswaApp(
+    modifier : Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+) {}
