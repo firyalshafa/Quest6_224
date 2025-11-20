e880b88126d88e3c0721c523830970eca17b01dd
firyalshafa <161413463+firyalshafa@users.noreply.github.com>
2025-11-20 15:31:06 +0700

    Create TampilSiswa UI composable

---
+++ app/src/main/java/com/example/praktikum7/view/uicontroller/TampilSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.praktikum7.view.uicontroller
+
+
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.res.stringResource
+import java.lang.reflect.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TampilSiswa() {
+
+}
